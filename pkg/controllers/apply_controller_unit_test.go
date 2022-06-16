8ba6d481feebbf100df7f27cf98ecbd0359f116c
Youn Jae Kim <younjaekim@microsoft.com>
2022-06-16 12:14:08 -0700

    Unit test for Reconcile work controller

--- pkg/controllers/apply_controller_unit_test.go
+++ pkg/controllers/apply_controller_unit_test.go
@@ -3,21 +3,29 @@ package controllers
 import (
 	"context"
 	"encoding/json"
+	"fmt"
 	"reflect"
 	"testing"
+	"time"
 
 	"github.com/crossplane/crossplane-runtime/pkg/test"
 	"github.com/pkg/errors"
 	"github.com/stretchr/testify/assert"
 	appsv1 "k8s.io/api/apps/v1"
 	v1 "k8s.io/api/core/v1"
+	apierrors "k8s.io/apimachinery/pkg/api/errors"
 	"k8s.io/apimachinery/pkg/api/meta"
 	metav1 "k8s.io/apimachinery/pkg/apis/meta/v1"
+	"k8s.io/apimachinery/pkg/apis/meta/v1/unstructured"
 	"k8s.io/apimachinery/pkg/runtime"
 	"k8s.io/apimachinery/pkg/runtime/schema"
-	utilrand "k8s.io/apimachinery/pkg/util/rand"
+	"k8s.io/apimachinery/pkg/types"
+	"k8s.io/apimachinery/pkg/util/rand"
+	"k8s.io/client-go/dynamic"
 	"k8s.io/client-go/dynamic/fake"
-	clienttesting "k8s.io/client-go/testing"
+	testingclient "k8s.io/client-go/testing"
+	ctrl "sigs.k8s.io/controller-runtime"
+	"sigs.k8s.io/controller-runtime/pkg/client"
 
 	workv1alpha1 "sigs.k8s.io/work-api/pkg/apis/v1alpha1"
 )
@@ -101,7 +109,7 @@ func TestApplyManifest(t *testing.T) {
 
 	// DynamicClients
 	clientFailDynamicClient := fake.NewSimpleDynamicClient(runtime.NewScheme())
-	clientFailDynamicClient.PrependReactor("get", "*", func(action clienttesting.Action) (handled bool, ret runtime.Object, err error) {
+	clientFailDynamicClient.PrependReactor("get", "*", func(action testingclient.Action) (handled bool, ret runtime.Object, err error) {
 		return true, nil, errors.New("Failed to apply an unstructrued object")
 	})
 
@@ -184,6 +192,278 @@ func TestApplyManifest(t *testing.T) {
 	}
 }
 
+func TestReconcile(t *testing.T) {
+	workNamespace := getRandomString()
+	workName := getRandomString()
+	appliedWorkName := getRandomString()
+	req := ctrl.Request{
+		NamespacedName: types.NamespacedName{
+			Namespace: workNamespace,
+			Name:      workName,
+		},
+	}
+	wrongReq := ctrl.Request{
+		NamespacedName: types.NamespacedName{
+			Namespace: getRandomString(),
+			Name:      getRandomString(),
+		},
+	}
+	invalidReq := ctrl.Request{
+		NamespacedName: types.NamespacedName{
+			Namespace: "",
+			Name:      "",
+		},
+	}
+
+	getMock := func(ctx context.Context, key client.ObjectKey, obj client.Object) error {
+		if key.Namespace != workNamespace {
+			return &apierrors.StatusError{
+				ErrStatus: metav1.Status{
+					Status: metav1.StatusFailure,
+					Reason: metav1.StatusReasonNotFound,
+				}}
+		}
+		o, _ := obj.(*workv1alpha1.Work)
+		*o = workv1alpha1.Work{
+			ObjectMeta: metav1.ObjectMeta{
+				Namespace:  workNamespace,
+				Name:       workName,
+				Finalizers: []string{"multicluster.x-k8s.io/work-cleanup"},
+			},
+			Spec: workv1alpha1.WorkSpec{Workload: workv1alpha1.WorkloadTemplate{Manifests: []workv1alpha1.Manifest{testManifest}}},
+		}
+		return nil
+	}
+
+	happyDeployment := appsv1.Deployment{
+		TypeMeta: metav1.TypeMeta{
+			Kind:       "Deployment",
+			APIVersion: "apps/v1",
+		},
+		ObjectMeta: metav1.ObjectMeta{
+			Name: "Deployment",
+			OwnerReferences: []metav1.OwnerReference{
+				{
+					APIVersion: workv1alpha1.GroupVersion.String(),
+					Kind:       "AppliedWork",
+					Name:       appliedWorkName,
+				},
+			},
+		},
+		Spec: appsv1.DeploymentSpec{
+			MinReadySeconds: 5,
+		},
+	}
+	rawHappyDeployment, _ := json.Marshal(happyDeployment)
+	happyManifest := workv1alpha1.Manifest{RawExtension: runtime.RawExtension{
+		Raw: rawHappyDeployment,
+	}}
+	_, happyDynamicClient, _ := createObjAndDynamicClient(happyManifest.Raw)
+
+	getMockAppliedWork := func(ctx context.Context, key client.ObjectKey, obj client.Object) error {
+
+		if key.Name != workName {
+			return &apierrors.StatusError{
+				ErrStatus: metav1.Status{
+					Status: metav1.StatusFailure,
+					Reason: metav1.StatusReasonNotFound,
+				}}
+		}
+		o, _ := obj.(*workv1alpha1.AppliedWork)
+		*o = workv1alpha1.AppliedWork{
+			ObjectMeta: metav1.ObjectMeta{
+				Name: appliedWorkName,
+			},
+			Spec: workv1alpha1.AppliedWorkSpec{
+				WorkName:      workNamespace,
+				WorkNamespace: workName,
+			},
+		}
+		return nil
+	}
+
+	clientFailDynamicClient := fake.NewSimpleDynamicClient(runtime.NewScheme())
+	clientFailDynamicClient.PrependReactor("get", "*", func(action testingclient.Action) (handled bool, ret runtime.Object, err error) {
+		return true, nil, errors.New("Failed to apply an unstructrued object")
+	})
+
+	testCases := map[string]struct {
+		reconciler ApplyWorkReconciler
+		req        ctrl.Request
+		wantErr    error
+	}{
+		"work cannot be retrieved, client failed due to client error": {
+			reconciler: ApplyWorkReconciler{
+				client: &test.MockClient{
+					MockGet: func(ctx context.Context, key client.ObjectKey, obj client.Object) error {
+						return fmt.Errorf("client failing")
+					},
+				},
+				spokeDynamicClient: fakeDynamicClient,
+				spokeClient:        &test.MockClient{},
+				restMapper:         testMapper{},
+			},
+			req:     invalidReq,
+			wantErr: errors.New("client failing"),
+		},
+		"work cannot be retrieved, client failed due to not found error": {
+			reconciler: ApplyWorkReconciler{
+				client: &test.MockClient{
+					MockGet: getMock,
+				},
+				spokeDynamicClient: fakeDynamicClient,
+				spokeClient:        &test.MockClient{},
+				restMapper:         testMapper{},
+			},
+			req:     wrongReq,
+			wantErr: nil,
+		},
+		"work without finalizer / no error": {
+			reconciler: ApplyWorkReconciler{
+				client: &test.MockClient{
+					MockGet: func(ctx context.Context, key client.ObjectKey, obj client.Object) error {
+						o, _ := obj.(*workv1alpha1.Work)
+						*o = workv1alpha1.Work{
+							ObjectMeta: metav1.ObjectMeta{
+								Namespace: workNamespace,
+								Name:      workName,
+							},
+						}
+						return nil
+					},
+				},
+				spokeDynamicClient: fakeDynamicClient,
+				spokeClient:        &test.MockClient{},
+				restMapper:         testMapper{},
+			},
+			req:     req,
+			wantErr: nil,
+		},
+		"work with non-zero deletion-timestamp / succeed": {
+			reconciler: ApplyWorkReconciler{
+				client: &test.MockClient{
+					MockGet: func(ctx context.Context, key client.ObjectKey, obj client.Object) error {
+						o, _ := obj.(*workv1alpha1.Work)
+						*o = workv1alpha1.Work{
+							ObjectMeta: metav1.ObjectMeta{
+								Namespace:         workNamespace,
+								Name:              workName,
+								Finalizers:        []string{"multicluster.x-k8s.io/work-cleanup"},
+								DeletionTimestamp: &metav1.Time{Time: time.Now()},
+							},
+						}
+						return nil
+					},
+				},
+				spokeDynamicClient: fakeDynamicClient,
+				spokeClient:        &test.MockClient{},
+				restMapper:         testMapper{},
+			},
+			req:     req,
+			wantErr: nil,
+		},
+		"Retrieving appliedwork fails": {
+			reconciler: ApplyWorkReconciler{
+				client: &test.MockClient{
+					MockGet: getMock,
+				},
+				spokeDynamicClient: fakeDynamicClient,
+				spokeClient: &test.MockClient{
+					MockGet: func(ctx context.Context, key client.ObjectKey, obj client.Object) error {
+						return &apierrors.StatusError{
+							ErrStatus: metav1.Status{
+								Status: metav1.StatusFailure,
+								Reason: metav1.StatusReasonNotFound,
+							}}
+					},
+				},
+				restMapper: testMapper{},
+			},
+			req:     req,
+			wantErr: errors.New("failed to get the appliedWork"),
+		},
+		"ApplyManifest fails": {
+			reconciler: ApplyWorkReconciler{
+				client: &test.MockClient{
+					MockGet: getMock,
+					MockStatusUpdate: func(ctx context.Context, obj client.Object, opts ...client.UpdateOption) error {
+						return nil
+					},
+				},
+				spokeDynamicClient: clientFailDynamicClient,
+				spokeClient: &test.MockClient{
+					MockGet: getMockAppliedWork,
+					MockStatusUpdate: func(ctx context.Context, obj client.Object, opts ...client.UpdateOption) error {
+						return nil
+					},
+				},
+				restMapper: testMapper{},
+			},
+			req:     req,
+			wantErr: errors.New("Failed to apply an unstructrued object"),
+		},
+		"client update fails": {
+			reconciler: ApplyWorkReconciler{
+				client: &test.MockClient{
+					MockGet: getMock,
+					MockStatusUpdate: func(ctx context.Context, obj client.Object, opts ...client.UpdateOption) error {
+						return errors.New("update work status failed")
+					},
+				},
+				spokeDynamicClient: clientFailDynamicClient,
+				spokeClient: &test.MockClient{
+					MockGet: getMockAppliedWork,
+				},
+				restMapper: testMapper{},
+			},
+			req:     req,
+			wantErr: errors.New("update work status failed"),
+		},
+		"Happy Path": {
+			reconciler: ApplyWorkReconciler{
+				client: &test.MockClient{
+					MockGet: getMock,
+					MockStatusUpdate: func(ctx context.Context, obj client.Object, opts ...client.UpdateOption) error {
+						return nil
+					},
+				},
+				spokeDynamicClient: happyDynamicClient,
+				spokeClient: &test.MockClient{
+					MockGet: getMockAppliedWork,
+				},
+				restMapper: testMapper{},
+			},
+			req:     req,
+			wantErr: nil,
+		},
+	}
+	for testName, testCase := range testCases {
+		t.Run(testName, func(t *testing.T) {
+			ctrlResult, err := testCase.reconciler.Reconcile(context.Background(), testCase.req)
+			if testCase.wantErr != nil {
+				assert.Containsf(t, err.Error(), testCase.wantErr.Error(), "incorrect error for Testcase %s", testName)
+			} else {
+				assert.Equalf(t, false, ctrlResult.Requeue, "incorrect ctrlResult for Testcase %s", testName)
+			}
+		})
+	}
+}
+
+func createObjAndDynamicClient(rawManifest []byte) (unstructured.Unstructured, dynamic.Interface, string) {
+	unstructuredObj := &unstructured.Unstructured{}
+	_ = unstructuredObj.UnmarshalJSON(rawManifest)
+	validSpecHash, _ := generateSpecHash(unstructuredObj)
+	unstructuredObj.SetAnnotations(map[string]string{"multicluster.x-k8s.io/spec-hash": validSpecHash})
+	dynamicClient := fake.NewSimpleDynamicClient(runtime.NewScheme())
+	dynamicClient.PrependReactor("get", "*", func(action testingclient.Action) (handled bool, ret runtime.Object, err error) {
+		return true, unstructuredObj, nil
+	})
+	dynamicClient.PrependReactor("patch", "*", func(action testingclient.Action) (handled bool, ret runtime.Object, err error) {
+		return true, unstructuredObj, nil
+	})
+	return *unstructuredObj, dynamicClient, validSpecHash
+}
+
 func getRandomString() string {
-	return utilrand.String(10)
+	return rand.String(10)
 }
