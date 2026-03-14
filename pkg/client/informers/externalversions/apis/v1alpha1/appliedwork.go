61a1de6b7937403e27f86267e7a5bc87c211b1ec
RainbowMango <qdurenhongcai@gmail.com>
2026-03-14 10:48:05 +0800

    Bump Kubernetes dependencies to v1.35.0 Update code generated files Bump golangci-lint to v2.8.0
    Signed-off-by: RainbowMango <qdurenhongcai@gmail.com>

--- pkg/client/informers/externalversions/apis/v1alpha1/appliedwork.go
+++ pkg/client/informers/externalversions/apis/v1alpha1/appliedwork.go
@@ -56,7 +56,7 @@ func NewAppliedWorkInformer(client versioned.Interface, resyncPeriod time.Durati
 // one. This reduces memory footprint and number of connections to the server.
 func NewFilteredAppliedWorkInformer(client versioned.Interface, resyncPeriod time.Duration, indexers cache.Indexers, tweakListOptions internalinterfaces.TweakListOptionsFunc) cache.SharedIndexInformer {
 	return cache.NewSharedIndexInformer(
-		&cache.ListWatch{
+		cache.ToListWatcherWithWatchListSemantics(&cache.ListWatch{
 			ListFunc: func(options v1.ListOptions) (runtime.Object, error) {
 				if tweakListOptions != nil {
 					tweakListOptions(&options)
@@ -81,7 +81,7 @@ func NewFilteredAppliedWorkInformer(client versioned.Interface, resyncPeriod tim
 				}
 				return client.MulticlusterV1alpha1().AppliedWorks().Watch(ctx, options)
 			},
-		},
+		}, client),
 		&pkgapisv1alpha1.AppliedWork{},
 		resyncPeriod,
 		indexers,
